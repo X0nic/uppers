216fd623f876e40db96a6a3ae2af97b9d5a1ea0a
Nathan Lee <nathan@globalphobia.com>
2011-03-19 21:56:02 -0600

    added new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uppers::Application.config.secret_token = '1a34f9d29bf952f751abc81ccae8c87f82d04f0a16fa6ee3d34fbe9e252b4ed639a52f2a07d3f095cee9f9f9991a0a8602c2229836dd6a9ee835996a6d4e3355'
