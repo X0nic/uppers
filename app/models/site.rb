8ac753b397dd0bd1aed61e6c1d93a3f3400568aa
Nathan Lee <nathan@globalphobia.com>
2011-04-20 08:24:53 -0600

    added temp id to sites

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,5 +1,12 @@
+require 'digest/sha1'
+
 class Site < ActiveRecord::Base
-  attr_accessible :uri, :email
+  attr_accessible :uri, :email, :temp_id
+
+  def after_initialize
+    self.temp_id = generate_temp_id
+  end
+  
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   uri_regex = /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/i
@@ -9,4 +16,9 @@ class Site < ActiveRecord::Base
 
   validates :uri, :presence => true,
                   :format => {:with => uri_regex}
+
+  def generate_temp_id
+    Digest::SHA1.hexdigest(Time.now.to_s.split(//).sort_by{rand}.join)
+  end
+  
 end
